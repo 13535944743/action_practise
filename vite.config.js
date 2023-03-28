117fe970dbe56fe2bca224243c4ff6c18a44a3a5
clz <1926389897@qq.com>
2023-03-29 00:10:03 +0800

    clz: change

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './'
+  base: './'    // 默认是根路径，修改为相对路径。否则部署github pages时，会去https://xxx.github.io/这个路径下找资源，结果会找不到。
 })
