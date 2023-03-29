34b782e523aa3b2979f7865084d2ad2f9c13aff2
clz <1926389897@qq.com>
2023-03-29 19:04:56 +0800

    clz: change

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="赤蓝紫123" />
+  <HelloWorld msg="赤蓝紫 CICD" />
 
 </template>
 
