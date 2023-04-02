423bf07ac2bc779e5be65b6bbeed85f50b46dcf4
赤蓝紫 <71619858+13535944743@users.noreply.github.com>
2023-04-02 22:15:13 +0800

    change to weather

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: './'    // 默认是根路径，修改为相对路径。否则部署github pages时，会去https://xxx.github.io/这个路径下找资源，结果会找不到。
-})
