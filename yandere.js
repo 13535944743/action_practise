8883a4cfd768e8aaa8970d694376d3323aec4a18
clz <1926389897@qq.com>
2023-04-07 21:58:53 +0800

    clz: test

--- yandere.js
+++ yandere.js
@@ -1,9 +1,9 @@
 import fs from 'fs';
 import fetch from 'node-fetch';
-import { SocksProxyAgent } from 'socks-proxy-agent';
+// import { SocksProxyAgent } from 'socks-proxy-agent';
 
-const proxy = 'socks://127.0.0.1:10808';;
-const agent = new SocksProxyAgent(proxy);
+// const proxy = 'socks://127.0.0.1:10808';;
+// const agent = new SocksProxyAgent(proxy);
 
 const slot = '{{clz}}';     // 插槽，用于替换
 const templateStr = `
@@ -30,7 +30,7 @@ const imgSource = 'https://danbooru.donmai.us/posts.json';     // 图源
 
 (async () => {
   const res = await fetch(`${imgSource}`, {
-    agent
+    // agent
   });
   
   const texts = await res.text();
@@ -50,7 +50,7 @@ const imgSource = 'https://danbooru.donmai.us/posts.json';     // 图源
     imgDomStr += `<img src="${imgUrlPrefix}/${filename}" alt="${img.tags}" title="${img.tags}"></img>`;
 
     const imgRes = await fetch(img.url, {
-      agent
+      // agent
     });
     
     const writeStream = fs.createWriteStream(`./img/${filename}`);
